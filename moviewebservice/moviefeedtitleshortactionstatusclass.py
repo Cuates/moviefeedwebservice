8d6099588a36503030e680ee4cd19c4d2b6b84e5
Cuates <48161526+Cuates@users.noreply.github.com>
2020-11-14 18:48:44 -0800

    Add files via upload

---
+++ moviewebservice/moviefeedtitleshortactionstatusclass.py
@@ -0,0 +1,384 @@
+##
+#        File: moviefeedtitleshortactionstatusclass.py
+#     Created: 11/10/2020
+#     Updated: 11/14/2020
+#  Programmer: Cuates
+#  Updated By: Cuates
+#     Purpose: Movie feed title short action status web service
+#     Version: 0.0.1 Python3
+##
+
+# Import modules
+import moviefeedwebserviceclass # movie feed web service class
+from flask import Flask, request, jsonify # Flask, request, jsonify
+from flask_restful import Resource #Api # flask_restful, api, resource
+import json # json
+
+# Class
+class MovieFeedTitleShortActionStatusClass(Resource):
+  # Constructor
+  def __init__(self):
+    pass
+
+  # Get method
+  def get(self):
+    # Initialize list, dictionary, and variables
+    resultDict = {}
+    returnDict = {}
+    codeVal = 200
+
+    # Create object of movie feed web service class
+    mfwsclass = moviefeedwebserviceclass.MovieFeedWebServiceClass()
+
+    # try to execute the command(s)
+    try:
+      # Store headers
+      wsHead = dict(request.headers)
+
+      # Set variable
+      #reqPath = request.path
+
+      # Check if headers were provided
+      webserviceHeaderResponse = mfwsclass._checkHeaders(wsHead)
+
+      # Check if element exists
+      if webserviceHeaderResponse.get('Status') != None:
+        # Check if status is success
+        if webserviceHeaderResponse['Status'] == 'Success':
+          # Set list
+          mandatoryParams = []
+
+          # Check payload
+          payloadResponse = mfwsclass._checkPayload(request.method, request.args, mandatoryParams)
+
+          # Check if element exists
+          if payloadResponse.get('Status') != None:
+            # Check if status is success
+            if payloadResponse['Status'] == 'Success':
+              # Initialize list
+              possibleParams = ['titlelong', 'titleshort', 'actionnumber', 'limit', 'sort']
+
+              # Extract movie feed
+              #resultDict = mfwsclass._extractMovieFeed('MariaDBSQLMovie', 'extracting', 'dbo.extractMediaFeed', 'extractMovieFeed', possibleParams, payloadResponse['Result'])
+              #resultDict = mfwsclass._extractMovieFeed('PGSQLMovie', 'extracting', 'dbo.extractMediaFeed', 'extractMovieFeed', possibleParams, payloadResponse['Result'])
+              #resultDict = mfwsclass._extractMovieFeed('MSSQLLMovie', 'extracting', 'dbo.extractMediaFeed', 'extractMovieFeed', possibleParams, payloadResponse['Result'])
+              resultDict = mfwsclass._extractMovieFeed('MSSQLWMovie', 'extracting', 'dbo.extractMediaFeed', 'extractMovieFeed', possibleParams, payloadResponse['Result'])
+
+              # Check if there is data
+              if resultDict:
+                # Loop through sub elements
+                for systemEntries in resultDict:
+                  # Check if elements exists
+                  if systemEntries.get('SError') == None:
+                      # Store Message
+                      returnDict = {'Status': 'Success', 'Message': 'Processed request', 'Count': len(resultDict), 'Result': resultDict}
+                  else:
+                    # Store Message
+                    returnDict = {'Status': systemEntries['SError'], 'Message': systemEntries['SMessage'], 'Count': 0, 'Result': []}
+
+                    # Set code
+                    codeVal = 500
+
+                    # Break out of loop
+                    break
+              else:
+                # Store Message
+                returnDict = {'Status': 'Success', 'Message': 'Processed request', 'Count': 0, 'Result': []}
+            else:
+              # Store Message
+              returnDict = {'Status': payloadResponse['Status'], 'Message': payloadResponse['Message'], 'Count': 0, 'Result': []}
+
+              # Store code
+              codeVal = 400
+          else:
+            # Store Message
+            returnDict = {'Status': 'Error', 'Message': 'Issue with payload check', 'Count': 0, 'Result': []}
+
+            # Store code
+            codeVal = 400
+        else:
+          # Store Message
+          returnDict = {'Status': webserviceHeaderResponse['Status'], 'Message': webserviceHeaderResponse['Message'], 'Count': 0, 'Result': []}
+
+          # Store code
+          codeVal = 400
+      else:
+        # Store Message
+        returnDict = {'Status': 'Error', 'Message': 'Issue with header check', 'Count': 0, 'Result': []}
+
+        # Store code
+        codeVal = 400
+    # Catch exceptions
+    except Exception as e:
+      # Store Message
+      returnDict = {'Status': 'Error', 'Message': 'GET not implemented properly', 'Count': 0, 'Result': []}
+
+      # Log message
+      mfwsclass._setLogger('GET ' + str(e))
+
+    # Return dictionary with code
+    return returnDict, codeVal
+
+  # Post method
+  def post(self):
+    # Initialize list, dictionary, and variables
+    resultDict = {}
+    returnDict = {}
+    codeVal = 200
+
+    # Create object of movie feed web service class
+    mfwsclass = moviefeedwebserviceclass.MovieFeedWebServiceClass()
+
+    # try to execute the command(s)
+    try:
+      # Store headers
+      wsHead = dict(request.headers)
+
+      # Set variable
+      #reqPath = request.path
+
+      # Check if headers were provided
+      webserviceHeaderResponse = mfwsclass._checkHeaders(wsHead)
+      
+      # Check if element exists
+      if webserviceHeaderResponse.get('Status') != None:
+        # Check if status is success
+        if webserviceHeaderResponse['Status'] == 'Success':
+          # Set list
+          mandatoryParams = ['titlelong', 'titleshort', 'publishdate', 'actionstatus']
+
+          # Check payload
+          payloadResponse = mfwsclass._checkPayload(request.method, request.data, mandatoryParams)
+
+          # Check if element exists
+          if payloadResponse.get('Status') != None:
+            # Check if status is success
+            if payloadResponse['Status'] == 'Success':
+              # Initailize list
+              possibleParams = ['titlelong', 'titleshort', 'publishdate', 'actionstatus']
+
+              # Insert movie feed
+              #resultDict = mfwsclass._insertupdatedeleteMovieFeed('MariaDBSQLMovie', 'inserting', 'insertupdatedeletemediafeed', 'insertMovieFeed', possibleParams, payloadResponse['Result'])
+              #resultDict = mfwsclass._insertupdatedeleteMovieFeed('PGSQLMovie', 'inserting', 'insertupdatedeletemediafeed', 'insertMovieFeed', possibleParams, payloadResponse['Result'])
+              #resultDict = mfwsclass._insertupdatedeleteMovieFeed('MSSQLLMovie', 'inserting', 'dbo.insertupdatedeleteMediaFeed', 'insertMovieFeed', possibleParams, payloadResponse['Result'])
+              resultDict = mfwsclass._insertupdatedeleteMovieFeed('MSSQLWMovie', 'inserting', 'dbo.insertupdatedeleteMediaFeed', 'insertMovieFeed', possibleParams, payloadResponse['Result'])
+
+              # Loop through sub elements
+              for systemEntries in resultDict:
+                # Check if elements exists
+                if systemEntries.get('SError') == None:
+                  # Store Message
+                  returnDict = {'Status': 'Success', 'Message': 'Processed request)', 'Count': len(resultDict), 'Result': resultDict}
+                else:
+                  # Store Message
+                  returnDict = {'Status': systemEntries['SError'], 'Message': systemEntries['SMessage'], 'Count': 0, 'Result': []}
+
+                  # Break out of loop
+                  break
+            else:
+              # Store Message
+              returnDict = {'Status': payloadResponse['Status'], 'Message': payloadResponse['Message'], 'Count': 0, 'Result': []}
+
+              # Store code
+              codeVal = 400
+          else:
+            # Store Message
+            returnDict = {'Status': 'Error', 'Message': 'Issue with payload check', 'Count': 0, 'Result': []}
+
+            # Store code
+            codeVal = 400
+        else:
+          # Store Message
+          returnDict = {'Status': webserviceHeaderResponse['Status'], 'Message': webserviceHeaderResponse['Message'], 'Count': 0, 'Result': []}
+      else:
+        # Store Message
+        returnDict = {'Status': 'Error', 'Message': 'Issue with header check', 'Count': 0, 'Result': []}
+    # Catch exceptions
+    except Exception as e:
+      # Store Message
+      returnDict = {'Status': 'Error', 'Message': 'POST not implemented properly', 'Count': 0, 'Result': []}
+
+      # Set code
+      codeVal = 500
+
+      # Log message
+      mfwsclass._setLogger('POST ' + str(e))
+
+    # Return dictionary with code
+    return returnDict, codeVal
+
+  # Put method
+  def put(self):
+    # Initialize list, dictionary, and variables
+    resultDict = {}
+    returnDict = {}
+    codeVal = 200
+
+    # Create object of movie feed web service class
+    mfwsclass = moviefeedwebserviceclass.MovieFeedWebServiceClass()
+
+    # try to execute the command(s)
+    try:
+      # Store headers
+      wsHead = dict(request.headers)
+
+      ## Set variable
+      #reqPath = request.path
+
+      # Check if headers were provided
+      webserviceHeaderResponse = mfwsclass._checkHeaders(wsHead)
+
+      # Check if element exists
+      if webserviceHeaderResponse.get('Status') != None:
+        # Check if status is success
+        if webserviceHeaderResponse['Status'] == 'Success':
+          # Set list
+          mandatoryParams = ['titleshort', 'actionstatus']
+
+          # Check payload
+          payloadResponse = mfwsclass._checkPayload(request.method, request.data, mandatoryParams)
+
+          # Check if element exists
+          if payloadResponse.get('Status') != None:
+            # Check if status is success
+            if payloadResponse['Status'] == 'Success':
+              # Initailize list
+              possibleParams = ['titleshort', 'actionstatus']
+
+              # Update movie feed
+              #resultDict = mfwsclass._insertupdatedeleteMovieFeed('MariaDBSQLMovie', 'updating', 'insertupdatedeletemediafeed', 'updateMovieFeedTitleShortActionStatus', possibleParams, payloadResponse['Result'])
+              #resultDict = mfwsclass._insertupdatedeleteMovieFeed('PGSQLMovie', 'updating', 'insertupdatedeletemediafeed', 'updateMovieFeedTitleShortActionStatus', possibleParams, payloadResponse['Result'])
+              #resultDict = mfwsclass._insertupdatedeleteMovieFeed('MSSQLLMovie', 'updating', 'dbo.insertupdatedeleteMediaFeed', 'updateMovieFeedTitleShortActionStatus', possibleParams, payloadResponse['Result'])
+              resultDict = mfwsclass._insertupdatedeleteMovieFeed('MSSQLWMovie', 'updating', 'dbo.insertupdatedeleteMediaFeed', 'updateMovieFeedTitleShortActionStatus', possibleParams, payloadResponse['Result'])
+
+              # Loop through sub elements
+              for systemEntries in resultDict:
+                # Check if elements exists
+                if systemEntries.get('SError') == None:
+                  # Store Message
+                  returnDict = {'Status': 'Success', 'Message': 'Processed request', 'Count': len(resultDict), 'Result': resultDict}
+                else:
+                  # Store Message
+                  returnDict = {'Status': systemEntries['SError'], 'Message': systemEntries['SMessage'], 'Count': 0, 'Result': []}
+
+                  # Break out of loop
+                  break
+            else:
+              # Store Message
+              returnDict = {'Status': payloadResponse['Status'], 'Message': payloadResponse['Message'], 'Count': 0, 'Result': []}
+
+              # Store code
+              codeVal = 400
+          else:
+            # Store Message
+            returnDict = {'Status': 'Error', 'Message': 'Issue with payload check', 'Count': 0, 'Result': []}
+
+            # Store code
+            codeVal = 400
+        else:
+          # Store Message
+          returnDict = {'Status': webserviceHeaderResponse['Status'], 'Message': webserviceHeaderResponse['Message'], 'Count': 0, 'Result': []}
+      else:
+        # Store Message
+        returnDict = {'Status': 'Error', 'Message': 'Issue with header check', 'Count': 0, 'Result': []}
+    # Catch exceptions
+    except Exception as e:
+      # Store Message
+      returnDict = {'Status': 'Error', 'Message': 'PUT not implemented properly', 'Count': 0, 'Result': []}
+
+      # Set code
+      codeVal = 500
+
+      # Log message
+      mfwsclass._setLogger('PUT ' + str(e))
+
+    # Return dictionary with code
+    return returnDict, codeVal
+
+  # Delete method
+  def delete(self):
+    # Initialize list, dictionary, and variables
+    resultDict = {}
+    returnDict = {}
+    codeVal = 200
+
+    # Create object of movie feed web service class
+    mfwsclass = moviefeedwebserviceclass.MovieFeedWebServiceClass()
+
+    # try to execute the command(s)
+    try:
+      # Store headers
+      wsHead = dict(request.headers)
+
+      ## Set variable
+      #reqPath = request.path
+
+      # Check if headers were provided
+      webserviceHeaderResponse = mfwsclass._checkHeaders(wsHead)
+      
+      # Check if element exists
+      if webserviceHeaderResponse.get('Status') != None:
+        # Check if status is success
+        if webserviceHeaderResponse['Status'] == 'Success':
+          # Set list
+          mandatoryParams = ['titleshort']
+
+          # Check payload
+          payloadResponse = mfwsclass._checkPayload(request.method, request.data, mandatoryParams)
+
+          # Check if element exists
+          if payloadResponse.get('Status') != None:
+            # Check if status is success
+            if payloadResponse['Status'] == 'Success':
+              # Initailize list
+              possibleParams = ['titleshort']
+
+              # Delete movie feed
+              #resultDict = mfwsclass._insertupdatedeleteMovieFeed('MariaDBSQLMovie', 'deleting', 'insertupdatedeletemediafeed', 'updateMovieFeedTitleShort', possibleParams, payloadResponse['Result'])
+              #resultDict = mfwsclass._insertupdatedeleteMovieFeed('PGSQLMovie', 'deleting', 'insertupdatedeletemediafeed', 'updateMovieFeedTitleShort', possibleParams, payloadResponse['Result'])
+              #resultDict = mfwsclass._insertupdatedeleteMovieFeed('MSSQLLMovie', 'deleting', 'dbo.insertupdatedeleteMediaFeed', 'updateMovieFeedTitleShort', possibleParams, payloadResponse['Result'])
+              resultDict = mfwsclass._insertupdatedeleteMovieFeed('MSSQLWMovie', 'deleting', 'dbo.insertupdatedeleteMediaFeed', 'updateMovieFeedTitleShort', possibleParams, payloadResponse['Result'])
+
+              # Loop through sub elements
+              for systemEntries in resultDict:
+                # Check if elements exists
+                if systemEntries.get('SError') == None:
+                  # Store Message
+                  returnDict = {'Status': 'Success', 'Message': 'Processed request', 'Count': len(resultDict), 'Result': resultDict}
+                else:
+                  # Store Message
+                  returnDict = {'Status': systemEntries['SError'], 'Message': systemEntries['SMessage'], 'Count': 0, 'Result': []}
+
+                  # Break out of loop
+                  break
+            else:
+              # Store Message
+              returnDict = {'Status': payloadResponse['Status'], 'Message': payloadResponse['Message'], 'Count': 0, 'Result': []}
+
+              # Store code
+              codeVal = 400
+          else:
+            # Store Message
+            returnDict = {'Status': 'Error', 'Message': 'Issue with payload check', 'Count': 0, 'Result': []}
+
+            # Store code
+            codeVal = 400
+        else:
+          # Store Message
+          returnDict = {'Status': webserviceHeaderResponse['Status'], 'Message': webserviceHeaderResponse['Message'], 'Count': 0, 'Result': []}
+      else:
+        # Store Message
+        returnDict = {'Status': 'Error', 'Message': 'Issue with header check', 'Count': 0, 'Result': []}
+    # Catch exceptions
+    except Exception as e:
+      # Store Message
+      returnDict = {'Status': 'Error', 'Message': 'DELETE not implemented properly', 'Count': 0, 'Result': []}
+
+      # Set code
+      codeVal = 500
+
+      # Log message
+      mfwsclass._setLogger('DELETE ' + str(e))
+
+    # Return dictionary with code
+    return returnDict, codeVal
---
+++ moviewebservice/moviefeedwebservice.py
@@ -0,0 +1,81 @@
+##
+#        File: moviefeedwebservice.py
+#     Created: 11/10/2020
+#     Updated: 11/14/2020
+#  Programmer: Cuates
+#  Updated By: Cuates
+#     Purpose: Movie feed web service
+#     Version: 0.0.1 Python3
+##
+
+# Import modules
+from flask import Flask, request, jsonify # Flask, request, jsonify
+from flask_restful import Api # flask_restful, api
+import json # json
+import moviefeedwebserviceclass # movie feed web service class
+from moviefeedclass import MovieFeedClass # movie feed web service class
+from moviefeedtitleshortclass import MovieFeedTitleShortClass # movie feed title short web service class
+from moviefeedtitleshortactionstatusclass import MovieFeedTitleShortActionStatusClass # movie feed title short action status web service class
+
+# Create objects
+app = Flask(__name__)
+api = Api(app)
+
+# Set object
+mfwsclass = moviefeedwebserviceclass.MovieFeedWebServiceClass()
+
+# try to execute the command(s)
+try:
+  # Error handler
+  @app.errorhandler(Exception)
+  def errorHandling(eh):
+    # Initialize variables
+    messageVal = ''
+    codeVal = 500
+
+    # try to execute the command(s)
+    try:
+      # Set message
+      messageVal = str(eh.code) + ' ' + str(eh.name) + ' ' + str(eh.description)
+
+      # Set code
+      codeVal = eh.code
+    # Catch exceptions
+    except Exception as e:
+      # Set message
+      messageVal = 'ErrorHandler'
+
+      # Log message
+      tfwsclass._setLogger('ErrorHandler ' + str(e))
+
+    # Return message
+    return {'Status': 'Error', 'Message': messageVal, 'Count': 0, 'Result': []}, codeVal
+
+  # Store possible methods
+  validMethods = ['GET', 'POST', 'PUT', 'DELETE']
+
+  # Before request
+  @app.before_request
+  def before_request_func():
+    # Check if request method is in the list
+    if request.method not in validMethods:
+      # Return message
+      return {'Status': 'Error', 'Message': 'Method invalid or not implemented', 'Count': 0, 'Result': []}, 501
+
+  # Add resource for api web service call movie feed
+  api.add_resource(MovieFeedClass, '/api/moviefeed', methods = validMethods)
+
+  # Add resource for additional api web service call movie feed title short
+  api.add_resource(MovieFeedTitleShortClass, '/api/moviefeed/titleshort', methods = ['PUT', 'DELETE'])
+
+  # Add resource for additional api web service call movie feed title short action status
+  api.add_resource(MovieFeedTitleShortActionStatusClass, '/api/moviefeed/titleshortactionstatus', methods = ['PUT'])
+# Catch exceptions
+except Exception as e:
+  # Log message
+  mfwsclass._setLogger('Issue executing main PY file ' + str(e))
+
+# Run program
+if __name__ == '__main__':
+  # Run app
+  app.run(host='127.0.0.1', port=5001, debug=True, threaded=True)
